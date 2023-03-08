2ac59ac3197afa1cac2069d8c2d35f5e9f7da9f4
Le Dinh Nguyen <ledinhnguyen00@gmail.com>
2023-03-08 13:22:30 +0700

    update

---
+++ DataStructure/SinglyLinkedList.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next = nullptr;
+    Node(int data)
+    {
+        value = data;
+    }
+};
+
+void createNode(Node *node)
+{
+}
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertionLast(Node *head, int data)
+{
+    Node *current = head;
+    Node *newNode = new Node(data);
+    while (current->next != nullptr)
+    {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+void deletionLast(Node *head)
+{
+    Node *last = head;
+    while (last->next->next != nullptr)
+    {
+        last = last->next;
+    }
+    last->next = nullptr;
+}
+
+void traverse()
+{
+}
+
+int main()
+{
+    Node *one = new Node(1);
+    Node *two = new Node(2);
+    Node *three = new Node(3);
+
+    Node *head = one;
+    one->next = two;
+    two->next = three;
+
+    cout << "Test case: ";
+    printList(head);
+    cout << endl;
+
+    cout << "Insertion: ";
+    insertionLast(head, 4);
+    insertionLast(head, 5);
+    printList(head);
+    cout << endl;
+
+    cout << "Deletion: ";
+    deletionLast(head);
+    printList(head);
+    cout << endl;
+
+    return 0;
+}
